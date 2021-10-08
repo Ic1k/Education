0f92e7b06a179da6a846d320f01370779b54fa3e
Kravtsov Andrey <ansekr@gmail.com>
2021-10-08 22:44:15 +0700

    1.2

--- Project 1/main.c
+++ Project 1/main.c
@@ -5,6 +5,7 @@ int main() {
     int z = 0;
     int sumnum;
     double allsum = 0;
+    double allsum17 = 0;
     for (int i = 0; i < 1000; i++)
         if((i % 2) != 0) {
             list[z] = i * i * i;
@@ -22,7 +23,18 @@ int main() {
         if(sumnum % 7 == 0)
             allsum += list[i];
     }
-
-    printf("%.lf", allsum);
+    for (int i = 0; i < z; i++) {
+        sumnum = 0;
+        for (int num = (list[i] + 0.17) * 100; num > 0;) {
+            if (num > 9)
+                sumnum += num % 10;
+            else
+                sumnum += num;
+            num = num / 10;
+        }
+        if(sumnum % 7 == 0)
+            allsum17 += list[i];
+    }
+    printf("%.lf %.lf", allsum, allsum17);
     return 0;
 }
